6b4ea6236116898f452d6aa32786523a42209978
lucas <lucas.devtibetano@gmail.com>
2025-08-15 12:13:10 -0300

    Added custom exceptions. Added custom exception to GlobalExceptionHandler.

--- anigame-api/src/main/java/com/anigame/api/exception/GlobalExceptionHandler.java
+++ anigame-api/src/main/java/com/anigame/api/exception/GlobalExceptionHandler.java
@@ -28,4 +28,24 @@ public class GlobalExceptionHandler {
                 request.getRequestURI());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorized(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<String> handleForbidden(ForbiddenException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflict(ConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
 }
--- anigame-api/src/main/java/com/anigame/api/exception/ResourceNotFoundException.java
+++ anigame-api/src/main/java/com/anigame/api/exception/ResourceNotFoundException.java
@@ -4,4 +4,7 @@ public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
     }
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s with %s '%s' not found.", resourceName, fieldName, fieldValue));
+    }
 }
